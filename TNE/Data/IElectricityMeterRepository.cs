6c541ff57553e8885fa041471de287dd28b2d332
Dmitry Petelko <dpetelko@gmail.com>
2020-10-03 12:56:09 +0300

    Added ElectricityRepository

---
+++ TNE/Data/IElectricityMeterRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TNE.Dtos;
+using TNE.Models;
+using TNE.Services;
+
+namespace TNE.Data
+{
+    public interface IElectricityMeterRepository : IService<ElectricityMeter>
+    {
+        Task<bool> SetStatus(Guid id, Status newStatus);
+        Task<ElectricityMeterDto> GetDtoByIdAsync(Guid Id);
+        Task<List<ElectricityMeterDto>> GetAllDtoAsync();
+        Task<List<ElectricityMeterDto>> GetAllDtoByStatusAsync(Status status);
+    }
+}
---
+++ TNE/Data/Implementations/ElectricityMeterRepositoryImpl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TNE.Data.Implementations
+{
+    public class ElectricityMeterRepositoryImpl
+    {
+        private readonly DatabaseContext _context;
+
+        public ElectricityMeterRepositoryImpl(DatabaseContext context) { _context = context; }
+    }
+}
