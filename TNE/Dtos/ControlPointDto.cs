88d7617b98c157f7962e4020753b8fd7c43ddd42
Dmitry Petelko <dpetelko@gmail.com>
2020-10-04 11:43:59 +0300

    Refactoring

--- TNE/Dtos/ControlPointDto.cs
+++ TNE/Dtos/ControlPointDto.cs
@@ -17,9 +17,24 @@ namespace TNE.Dtos
         [Required]
         public Guid ProviderId { get; set; }
         public string ProviderName { get; set; }
-        public ElectricityMeterDto ElectricityMeter { get; set; }
-        public CurrentTransformerDto CurrentTransformer { get; set; }
-        public VoltageTransformerDto VoltageTransformer { get; set; }
+        [Required]
+        public Guid ElectricityMeterId { get; set; }
+        public string ElectricityMeterNumber { get; set; }
+        public string ElectricityMeterType { get; set; }
+        [Required]
+        public DateTime ElectricityMeterVerificationDate { get; set; }
+        [Required]
+        public Guid CurrentTransformerId { get; set; }
+        public string CurrentTransformerNumber { get; set; }
+        public string CurrentTransformerType { get; set; }
+        public DateTime CurrentTransformerVerificationDate { get; set; }
+        public int CurrentTransformerTransformationRate { get; set; }
+        [Required]
+        public Guid VoltageTransformerId { get; set; }
+        public string VoltageTransformerNumber { get; set; }
+        public string VoltageTransformerType { get; set; }
+        public DateTime VoltageTransformerVerificationDate { get; set; }
+        public int VoltageTransformerTransformationRate { get; set; }
 
         public ControlPointDto() { }
 
@@ -28,11 +43,26 @@ namespace TNE.Dtos
             Id = entity.Id;
             Name = entity.Name;
             Deleted = entity.Deleted;
+
             ProviderId = entity.Provider.Id;
             ProviderName = entity.Provider.Name;
-            CurrentTransformer = new CurrentTransformerDto(entity.CurrentTransformer);
-            VoltageTransformer = new VoltageTransformerDto(entity.VoltageTransformer);
-            ElectricityMeter = new ElectricityMeterDto(entity.ElectricityMeter);
+
+            CurrentTransformerId = entity.CurrentTransformer.Id;
+            CurrentTransformerNumber = entity.CurrentTransformer.Number;
+            CurrentTransformerType = entity.CurrentTransformer.Type;
+            CurrentTransformerVerificationDate = entity.CurrentTransformer.VerificationDate;
+            CurrentTransformerTransformationRate = entity.CurrentTransformer.TransformationRate;
+
+            VoltageTransformerId = entity.VoltageTransformer.Id;
+            VoltageTransformerNumber = entity.VoltageTransformer.Number;
+            VoltageTransformerType = entity.VoltageTransformer.Type;
+            VoltageTransformerVerificationDate = entity.VoltageTransformer.VerificationDate;
+            VoltageTransformerTransformationRate = entity.VoltageTransformer.TransformationRate;
+
+            ElectricityMeterId = entity.ElectricityMeter.Id;
+            ElectricityMeterNumber = entity.ElectricityMeter.Number;
+            ElectricityMeterType = entity.ElectricityMeter.Type;
+            ElectricityMeterVerificationDate = entity.ElectricityMeter.VerificationDate;
         }
     }
 }
