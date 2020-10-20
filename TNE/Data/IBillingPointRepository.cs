122f104172c2adf92a5b13aeb53c6b10b3ab4919
Dmitry Petelko <dpetelko@gmail.com>
2020-10-20 11:18:48 +0300

    Added documentation for BillingPointService

--- TNE/Data/IBillingPointRepository.cs
+++ TNE/Data/IBillingPointRepository.cs
@@ -10,7 +10,12 @@ namespace TNE.Data
 {
     public interface IBillingPointRepository : IRepository<BillingPoint>
     {
-        Task<BillingPointDto> GetDtoByIdAsync(Guid Id);
+        
+        Task<BillingPointDto> GetDtoByIdAsync(Guid id);
+        /// <summary>
+        /// Get all BillingPointsDto. 
+        /// Returns list of BillingPoints or EMPTY List, if no BillingPoints are found
+        /// </summary>
         Task<List<BillingPointDto>> GetAllDtoAsync();
         Task<List<BillingPointDto>> GetAllDtoByControlPointIdAsync(Guid id);
         Task<List<BillingPointDto>> GetAllDtoByDeliveryPointIdAsync(Guid id);
--- TNE/Services/IBillingPointService.cs
+++ TNE/Services/IBillingPointService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TNE.Data.Exceptions;
 using TNE.Dtos;
 using TNE.Dtos.SearchFilters;
 using TNE.Models;
@@ -13,8 +14,9 @@ namespace TNE.Services
     public interface IBillingPointService : IService<BillingPoint>
     {
         /// <summary>
-        /// Returns a specific BillingPoint by ID 
+        /// Returns a specific BillingPoint by ID
         /// </summary>
+        /// <exception cref="EntityNotFoundException">If no BillingPoints are found</exception>
         Task<BillingPointDto> GetDtoByIdAsync(Guid id);
 
         /// <summary>
@@ -27,25 +29,29 @@ namespace TNE.Services
         /// Creates a BillingPoint. 
         /// Returns a newly created BillingPoint
         /// </summary>
+        /// <exception cref="InvalidEntityException">If BillingPoints are invalid, e.g. ID is not Guid.Empty</exception>
         Task<BillingPointDto> CreateAsync(BillingPointDto dto);
 
         /// <summary>
         /// Updates a BillingPoint. Returns a updated BillingPoint
         /// </summary>
+        /// <exception cref="InvalidEntityException">If BillingPoints are invalid, e.g. ID is Guid.Empty</exception>
+        /// <exception cref="EntityNotFoundException">If BillingPoints are not found with this ID</exception>
+
         Task<BillingPointDto> UpdateAsync(BillingPointDto dto);
 
         /// <summary>
-        /// Returns list of BillingPoints by ControlPoint ID 
+        /// Returns list of BillingPoints by ControlPoint ID or EMPTY List, if no BillingPoints are found
         /// </summary>
         Task<List<BillingPointDto>> GetAllDtoByControlPointIdAsync(Guid id);
 
         /// <summary>
-        /// Returns list of BillingPoints by DeliveryPoint ID 
+        /// Returns list of BillingPoints by DeliveryPoint ID or EMPTY List, if no BillingPoints are found
         /// </summary>
         Task<List<BillingPointDto>> GetAllDtoByDeliveryPointIdAsync(Guid id);
 
         /// <summary>
-        /// Returns list list of BillingPoints by BillingPointFilter 
+        /// Returns list list of BillingPoints by BillingPointFilter or EMPTY List, if no BillingPoints are found
         /// </summary>
         Task<List<BillingPointDto>> GetAllDtoByFilterAsync(BillingPointFilter filter);
     }
