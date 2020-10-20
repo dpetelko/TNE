1cf2e4120e0e766bb2b40469c2d9180a2b294761
Dmitry Petelko <dpetelko@gmail.com>
2020-10-20 12:24:07 +0300

    Added documentation...

--- TNE/Services/IService.cs
+++ TNE/Services/IService.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using TNE.Data.Exceptions;
 
 namespace TNE.Services
 {
     public interface IService<T>
     {
+        /// <summary>
+        /// Returns a specific <typeparamref name="T"/> by ID
+        /// </summary>
+        /// <exception cref="EntityNotFoundException">If no <typeparamref name="T"/>s are found</exception>
         Task<T> GetByIdAsync(Guid id);
+        
+        /// <summary>
+        /// Returns a specific <typeparamref name="T"/> by ID
+        /// </summary>
+        /// <exception cref="EntityNotFoundException">If no <typeparamref name="T"/>s are found</exception>
         T GetById(Guid id);
+        
+        /// <summary>
+        /// Check exists <typeparamref name="T"/> by ID
+        /// </summary>
+        /// <exception cref="EntityNotFoundException">If no <typeparamref name="T"/>s are found</exception>
         void CheckExistsById(Guid id);
+        
+        /// <summary>
+        /// Check unique field <typeparam name="T"/>
+        /// </summary>
         bool IsFieldUnique(Guid id, string fieldName, object fieldValue);
     }
 }
